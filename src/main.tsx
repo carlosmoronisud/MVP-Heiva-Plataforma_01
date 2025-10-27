e78f9473c2137ec8431ca749eae526a858d54bf5
Carlos Moroni Garcia <carlosmoronisud@gmail.com>
2025-10-27 17:45:03 -0300

    deploy vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 
-const BASE_PATH = import.meta.env.BASE_URL;
+// const BASE_PATH = import.meta.env.BASE_URL;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={BASE_PATH}>
+    {/* <BrowserRouter basename={BASE_PATH}> */}
     <HelmetProvider>
   
       <App />
             
     </HelmetProvider>
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </StrictMode>
   
 )
